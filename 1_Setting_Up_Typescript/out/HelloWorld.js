cad553cdd3345a400442ba5f1e939c82c8b17701
Robert Nițu <niturobert02@yahoo.com>
2021-11-06 17:06:20 +0200

    Moved code from HelloWorld.ts to Main.ts

--- 1_Setting_Up_Typescript/out/HelloWorld.js
+++ 1_Setting_Up_Typescript/out/HelloWorld.js
@@ -1,5 +1,4 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 function hello(string) {
     console.log("Hello " + string + "!");
 }
@@ -11,17 +10,3 @@ function printName(stark) {
 }
 printName({ name: "Ana" });
 // printName({prenume: "Cameliu"});
-const Point_1 = require("./Point");
-const Point3D_1 = require("./Point3D");
-var point = new Point_1.Point(69, 69);
-var point2 = new Point_1.Point();
-console.log(point);
-point.printPoint();
-point2.printPoint();
-var point3D = new Point3D_1.Point3D(1, 1, 2); // [super] new Point(1, 1) + z = 2
-point3D.printPoint();
-var points = [new Point_1.Point(1, 1), new Point3D_1.Point3D(2, 3, 5), new Point_1.Point(8, 13)];
-points.forEach(point => {
-    console.log("* Point from forEach");
-    point.printPoint();
-});
