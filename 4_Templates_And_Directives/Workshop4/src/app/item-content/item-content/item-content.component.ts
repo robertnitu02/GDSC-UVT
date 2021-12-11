2d62b00453009d938850b777e3b6e4bdfab23356
Robert Nițu <niturobert02@yahoo.com>
2021-12-11 23:13:32 +0200

    Added templates work

--- 4_Templates_And_Directives/Workshop4/src/app/item-content/item-content/item-content.component.ts
+++ 4_Templates_And_Directives/Workshop4/src/app/item-content/item-content/item-content.component.ts
@@ -3,13 +3,35 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-item-content',
   templateUrl: './item-content.component.html',
-  styleUrls: ['./item-content.component.scss']
+  styleUrls: ['./item-content.component.scss'],
 })
 export class ItemContentComponent implements OnInit {
+  isLoggedIn = false;
+  listaVocale = ['a', 'e', 'i', 'o', 'u'];
+  listaConsoane = [
+    'b',
+    'c',
+    'd',
+    'f',
+    'g',
+    'h',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z',
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
