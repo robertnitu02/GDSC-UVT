0014bc6a4fbccc0f30de82893c395adf4b4989bf
Robert Nițu <niturobert02@yahoo.com>
2021-12-20 15:03:58 +0200

    Added work with routes

--- 5_Routing/Workshop5/src/app/app-routing.module.ts
+++ 5_Routing/Workshop5/src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
+import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '', pathMatch: 'full' },
+  { path: 'home', component: HomePageComponent, pathMatch: 'full' },
+  {
+    path: 'about',
+    children: [
+      { path: '', component: AboutPageComponent },
+      { path: 'contact', component: ContactPageComponent },
+    ],
+  },
+  { path: '**', component: NotFoundPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- 5_Routing/Workshop5/src/app/app.module.ts
+++ 5_Routing/Workshop5/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
+import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    AboutPageComponent,
+    ContactPageComponent,
+    NotFoundPageComponent
   ],
   imports: [
     BrowserModule,
