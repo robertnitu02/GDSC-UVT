d372afcd45ca4329a042fc160c19194c4d3b2621
Robert Nițu <niturobert02@yahoo.com>
2022-01-22 14:07:23 +0200

    Added src files

---
+++ Java Backend/1_Setting_Up/src/main/java/com/gdsc/server/GdcsApplication.java
@@ -0,0 +1,13 @@
+package com.gdsc.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdcsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GdcsApplication.class, args);
+    }
+
+}
